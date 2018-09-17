54912185042a432acca1b138a1fe69ff1e3d7c4e
PiLMee GaTes <pilmee@MacBook-Pro-de-PiLMee.local>
2018-09-17 07:16:48 -0500

    first commit

---
+++ projects/asmodeo/utils/src/lib/utils.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { UtilsComponent } from './utils.component';
+
+@NgModule({
+  imports: [
+  ],
+  declarations: [UtilsComponent],
+  exports: [UtilsComponent]
+})
+export class UtilsModule { }
