3b832c6239753c9a7d1665136fdae4a70aa31020
PiLMee GaTes <pilmee@MacBook-Pro-de-PiLMee.local>
2018-09-19 07:17:59 -0500

    fix(model-transform): Add validations and tests.

--- projects/asmodeo/utils/src/lib/model-transform/model-transform.ts
+++ projects/asmodeo/utils/src/lib/model-transform/model-transform.ts
@@ -1,11 +1,15 @@
 export class ModelTransform {
   public static Transform<O, D>(body: O | any, destinyModel: D | any) {
     try {
-      if (Array.isArray(body)) {
-        return body.map(data => new destinyModel(data));
+      if (body instanceof Object && destinyModel instanceof Object) {
+        if (Array.isArray(body)) {
+          return body.map(data => new destinyModel(data));
+        }
+
+        return new destinyModel(body);
       }
 
-      return new destinyModel(body);
+      throw new Error('Invalid operation.');
     } catch (error) {
       console.error(error);
     }
